bc4b1efe599c23684f4c96f12e857fe8974dc520
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-06-11 11:18:16 +0200

    Version 0.1.0

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='aes-vial',
+      version='0.1.0',
+      description='A simple wrapper for AES CTR mode.',
+      author='Peter Badida',
+      author_email='keyweeusr@gmail.com',
+      url='https://github.com/KeyWeeUsr/Vial',
+      packages=['vial'],
+      classifiers=[
+          'Topic :: Security :: Cryptography',
+          'License :: OSI Approved :: MIT License',
+      ],
+      license="License :: OSI Approved :: MIT License",
+      )
